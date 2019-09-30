67d259c00534191957d5eb3d7c21ca88c40c2e21
Rodney Reid <rodney.reid@gmail.com>
2019-09-30 14:34:42 -0500

    fixed json issue with agencies

--- refreshRoutes.js
+++ refreshRoutes.js
@@ -1,16 +1,28 @@
 // refreshRoutes makes a file of all the static route data -
 // stops, color, pid/pid names, and path lat/longs
 // @author Rodney Reid
+//
+// @todo - to incorporate more transit systems and a speedier setup:
+//
+// if TAGENCY (transit agency) isn't set in shell, but APIKEY is, we want to try
+// all agencies and determine which one it is.  This should be simple - request a routelist,
+// with their APIKEY, and see which one doesn't bomb out.
 // 
+// we should put something in the routes file that tells us which agency we're dealing with,
+// and also puts the center lat/long, initial zoom, and bounding lat/longs in an object.
+// 
+
 'use strict'
 
 const fetch = require('node-fetch')
 const fs = require('fs')
+const agencies = require('./agencies.json')
 const { PerformanceObserver, performance } = require('perf_hooks')
 const optimizeJSON = false // smaller precision lat/longs
 // Set the following in your enviroment; ( export MCTSAPIKEY="----some jumble of characters---"  in shell)
 const MCTSAPIKEY = process.env.MCTSAPIKEY // MILWAUKEE - get your own key: http://realtime.ridemcts.com/bustime/newDeveloper.jsp
 const CTAAPIKEY = process.env.CTAAPIKEY // CHICAGO - get your own key: http://www.ctabustracker.com/bustime/newDeveloper.jsp
+
 
 const BUSURL = 'http://realtime.ridemcts.com/bustime/api/v3/'
 const CTABUSURL = 'http://www.ctabustracker.com/bustime/api/v2/'
